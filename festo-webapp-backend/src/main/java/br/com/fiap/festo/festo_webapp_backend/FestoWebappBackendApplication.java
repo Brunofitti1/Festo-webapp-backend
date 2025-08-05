308b0d23f8aec5c08ed94aa6b2af681bc1bf7ebe
Bruno Morade <BMORADE@ford.com>
2025-08-05 16:23:38 -0300

    chore: first commit

---
+++ festo-webapp-backend/src/main/java/br/com/fiap/festo/festo_webapp_backend/FestoWebappBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.festo.festo_webapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestoWebappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestoWebappBackendApplication.class, args);
+	}
+
+}
---
+++ festo-webapp-backend/src/test/java/br/com/fiap/festo/festo_webapp_backend/FestoWebappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.festo.festo_webapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestoWebappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
